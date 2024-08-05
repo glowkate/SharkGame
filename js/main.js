f0d06c5228d80702354ad3be32b9a80970014865
spencers145 <smartypantsspencer@gmail.com>
2024-08-05 16:57:32 -0400

    Added Stormwalker's Twitter Link

--- js/main.js
+++ js/main.js
@@ -907,7 +907,7 @@ SharkGame.Button = {
 SharkGame.Changelog = {
     "<a href='https://github.com/Toby222/SharkGame'>New Frontiers</a> patch 20240803a": [
         "Added a new upgrade to the starter world: Crystal Scoops, to increase the speed of that world dramatically and make for slightly more interesting crab-to-laser-ray competition.",
-        "Added 4 new home message images, courtesy of stormwalker.",
+        "Added 4 new home message images, courtesy of <a href='https://x.com/stormwalker124'>stormwalker</a>.",
         "This happened silently in January, but: fixed the negative world time bug.",
         "Made a few changes to older ones.",
         "Fixed the shrimp home message image not appearing.",
