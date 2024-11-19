3290d599a1537317a5591288e9ed467443bc59cb
spencers145 <smartypantsspencer@gmail.com>
2024-11-19 07:29:52 -0500

    Changelog

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@ $.extend(SharkGame, {
     ],
     GAME_NAME: null,
     ACTUAL_GAME_NAME: "Shark Game",
-    VERSION: "20241106a",
+    VERSION: "20241119a",
     ORIGINAL_VERSION: 0.71,
     VERSION_NAME: "The Tempetuous Update",
     EPSILON: 1e-6, // floating point comparison is a joy
@@ -904,6 +904,10 @@ SharkGame.Button = {
 };
 
 SharkGame.Changelog = {
+    "<a href='https://github.com/Toby222/SharkGame'>New Frontiers</a> patch 20241119a": [
+        "Added another new sprite courtesy of <a href='https://www.youtube.com/@WorkerDroneMainBattleTrain'>Main Battle Train</a>.",
+        "Added another new sprite courtesy of <a href='https://github.com/glowkate'>Glowkate</a>.",
+    ],
     "<a href='https://github.com/Toby222/SharkGame'>New Frontiers</a> patch 20241106a": [
         "Added a new sprite courtesy of <a href='https://www.youtube.com/@WorkerDroneMainBattleTrain'>Main Battle Train</a>.",
     ],
